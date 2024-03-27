f6ad511ba589da9544ba421524fca71fe31e7f76
Omer Faruk Aydin <oemerfarukaydin97@gmail.com>
2024-03-27 17:59:31 +0300

    reference & primitve notu oluşturuldu

---
+++ Javascript/primitive & reference.js
@@ -0,0 +1,15 @@
+let a = 5;
+const myArray = [1,2,3];
+
+// primitive tipler değere göre değerlendirilir.
+// reference tipler bellekteki yerine göre değerlendirilir.
+
+
+
+if(a === 5){
+    console.log(true);
+}
+
+if(myArray === [1,2,3]){
+    console.log(true);
+}
