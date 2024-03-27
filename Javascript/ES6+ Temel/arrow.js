ff60c525e8763bd8e55cc1d028d070cc07fab7f3
Omer Faruk Aydin <oemerfarukaydin97@gmail.com>
2024-03-27 16:30:58 +0300

    ES6+ Arrow Function anlatıldı

---
+++ Javascript/ES6+ Temel/arrow.js
@@ -0,0 +1,29 @@
+// arrow fonksiyonlar
+
+// const merhaba = function(name){
+
+//     console.log("merhaba " + name);
+// };
+
+
+// const merhaba = (name, surname) => {
+//     console.log("Merhaba " + name + " " + surname);
+// }
+
+// const merhaba = (name, surname) => console.log("Merhaba " + name + " " + surname);
+
+// const merhaba = name => console.log("Merhaba " + name);
+
+// const merhaba = name => "Merhaba " + name;
+
+
+// console.log(merhaba("Ömer"));
+
+
+// const cube = (x) => {
+//     return x * x * x;
+// };
+
+// const cube = x => x * x * x;
+
+// console.log(cube(5));
