b4cc013dca3d38f155a2a237b63748fdb2a9590b
Omer Faruk Aydin <oemerfarukaydin97@gmail.com>
2024-03-28 16:24:12 +0300

    window dokümanı oluşturuldu.

---
+++ Javascript/window.js
@@ -0,0 +1,43 @@
+// console.log(window)
+
+// alert("Merhaba");
+
+// let result = window.confirm("Çıkmak istediğinize emin misiniz?");
+
+// if(result){
+//     alert("Çıkış yapılıyor...")
+// }
+// else{
+//     alert("Çıkılmıyor...")
+// }
+
+// let result = prompt("2 + 2:")
+
+// if(result == 4){
+//     alert("Doğru sonuç, tebrikler...")
+// }
+// console.log(typeof result)
+
+
+// let valueH = outerHeight;
+// let valueW = outerWidth;
+
+// let valueH = innerHeight;
+// let valueW = innerWidth;
+
+// console.log(valueH, valueW);
+
+// let value = scrollX;
+// let value = scrollY;
+
+
+// let value = location.host
+// let value = location.hostname
+// let value = location.port
+
+// console.log(value);
+
+
+// if(confirm("Sayfayı yenilemek istiyor musunuz?")){
+//     window.location.reload()
+// }
