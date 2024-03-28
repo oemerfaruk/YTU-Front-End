f6560c2684d03fffba273772354d0a255999f12a
Omer Faruk Aydin <oemerfarukaydin97@gmail.com>
2024-03-28 15:56:52 +0300

    scope dokümanı eklendi

---
+++ Javascript/scope.js
@@ -0,0 +1,54 @@
+// var value = "Ömer Faruk";
+// var value = "Aydın";
+// value = 26;
+
+// let value;
+// let value = 26; //Hatalı kullanm
+// value = 26;
+
+// const value = "YTÜ";
+
+// console.log(value);
+
+
+// var a = 1;
+// let b = 2;
+// const c = 3;
+
+// console.log(a,b,c);
+
+
+// function scope(){
+//     var a = 11;
+//     let b = 12;
+//     const c = 13;
+
+//     console.log(a,b,c);
+// }
+
+// scope();
+
+
+// if(a == 1){
+//     var a = 11;
+//     let b = 12;
+//     const c = 13;
+
+//     console.log(a,b,c);
+// }
+
+// console.log(a,b,c);
+
+
+var database = 'localhost';
+let db = 'localhost';
+
+console.log(database, db);
+
+
+if(true){
+    var database = "192.168.27.55";
+    let db = "1.1.1.1"
+}
+
+console.log(database, db);
